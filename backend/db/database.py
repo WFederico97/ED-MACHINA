72c29b8ea404a353088f306c7d6ede8abaf5c817
wfederico97 <wuthrich09@gmail.com>
2024-09-03 02:05:08 -0300

    REFACTORED - Db connection

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,23 +1,14 @@
 import os
 from typing import Annotated
 from fastapi import Depends
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='../config/env_prod.py')
 
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
 
-if None in [DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD]:
-    raise ValueError("Algunas variables de entorno no se cargaron correctamente.")
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = os.getenv("URL_DB", "postgresql://postgres:postgres@localhost:5432/leads_api")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
